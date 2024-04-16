f7f64962124209c8707875cfce029c00bc8b9503
82324361 <82324361@PTALI20307.animaedu.intranet>
2024-04-15 21:40:05 -0300

    Versão inicial.

---
+++ src/main/java/com/mycompany/biajava/BiaJava.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.biajava;
+
+/**
+ *
+ * @author 82324361
+ */
+public class BiaJava {
+
+    public static void main(String[] args) {
+        TelaElevador tela = new TelaElevador();
+        tela.setVisible(true);
+        System.out.println("Hello World!");
+    }
+}
